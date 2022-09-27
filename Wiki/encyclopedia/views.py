0bd3d65f9b30a72d2dffa383bc801cb875a3e543
mythic-hypercurve <67553039+mythic-hypercurve@users.noreply.github.com>
2022-09-27 13:58:15 +0100

    Work on routes for page loading

--- Wiki/encyclopedia/views.py
+++ Wiki/encyclopedia/views.py
@@ -1,10 +1,25 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+import markdown
 
 from . import util
 
+def markdownToHtml(entry):
+    content = util.get_entry(entry)
+    markdowner = markdown.Markdown()
+    if content == None:
+        return None
+    else:    
+        return markdowner.convert(entry)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def page(request, entry):
+    contentHtml = markdownToHtml(entry)
+    if contentHtml == None:
+        return render(request, "encyclopedia/error.html")
+    else:
+        return render(request, "encyclopedia/entry.html")  
